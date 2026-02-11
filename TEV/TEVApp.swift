4f9332f9ce948e0b9cffb7823cc71b51b450116a
RenatoB04 <renatob04@icloud.com>
2026-02-11 22:28:01 +0000

    Add initial files

---
+++ TEV/TEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEVApp.swift
+//  TEV
+//
+//  Created by VM on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
